8dd51e512ee7af5cba269261c5aa10a6ab4add2a
alexandreguedj <alexandreguedj@gmail.com>
2024-09-19 13:01:23 +0200

    test: add test on DomainFilter component

--- src/components/DomainFilter/DomainFilter.test.tsx
+++ src/components/DomainFilter/DomainFilter.test.tsx
@@ -1,17 +1,46 @@
-import DomainFilter from './DomainFilter';
-import { render, screen } from '@testing-library/react';
+import DomainFilter from './DomainFilter'
+import { render, screen } from '@testing-library/react'
 
-// TODO fix tests
+jest.mock('react-redux', () => ({
+	...jest.requireActual('react-redux'),
+	useSelector: () => ['FR_NK-WOL']
+}))
+
 describe('DomainFilter', () => {
-  it('should allow the user to filter', () => {
-    render(<DomainFilter />);
+	describe('rendering', () => {
+    it('should render without crashing',  () => {
+      // Given
+      render(<DomainFilter />)
 
-    expect(screen.getAllByRole('listbox')).toHaveLength(3);
-  })
+      // When
+      const result = screen.getByText('FR')
 
-  it('should render', async () => {
-    render(<DomainFilter />);
+      // Then
+      expect(result).toBeTruthy()
+    })
+    it('should have three selects', () => {
+      // Given
+      render(<DomainFilter />)
 
-    expect(await screen.findByText('do')).toBeTruthy();
-  });
+      // When
+      const selects = screen.getAllByRole('listbox')
+
+      // Then
+      expect(selects).toHaveLength(3)
+    })
+		it('should populate select with correct data', () => {
+			// Given
+			render(<DomainFilter />)
+
+			// When
+			const firstOption = screen.getByText('FR')
+			const secondOption = screen.getByText('NK')
+			const thirdOption = screen.getByText('WOL')
+
+			// Then
+			expect(firstOption).toBeTruthy()
+			expect(secondOption).toBeTruthy()
+			expect(thirdOption).toBeTruthy()
+		})
+	})
 })

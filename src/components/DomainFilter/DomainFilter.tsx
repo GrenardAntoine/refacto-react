e6ffb096f379edeecea5c0f437cd34a658440298
alexandreguedj <alexandreguedj@gmail.com>
2024-09-19 11:58:46 +0200

    refactor: remove the use of useState in DomainFilter.tsx

--- src/components/DomainFilter/DomainFilter.tsx
+++ src/components/DomainFilter/DomainFilter.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../redux/types'
-
-interface State {
-	countries: string[],
-	classifications: string[],
-	subClassifications: string[]
-}
 
 const DomainFilter = () => {
 	const domains = useSelector<RootState, string[]>(({ domains }) => domains)
 
-	const [state] = useState<State>({
-		countries: [...new Set(domains.map(domain => domain.substring(0, 2)))], // TODO make utils functions
-		classifications: domains.map(domain =>  domain.substring(3, 5)),
-		subClassifications: [...new Set(domains.map(domain => domain.substring(6)))],
-	})
+	const countries = [...new Set(domains.map(domain => domain.substring(0, 2)))] // TODO make utils functions
+	const classifications = [...new Set(domains.map(domain =>  domain.substring(3, 5)))]
+	const subClassifications = [...new Set(domains.map(domain => domain.substring(6)))]
 
 	return (<>
 		<select name="countries" multiple>
-			{state.countries.map(country => (
+			{countries.map(country => (
 				<option value={country} key={country}>{country}</option>
 			))}
 		</select>
 		<select name="classifications" multiple>
-			{state.classifications.map(classification => (
+			{classifications.map(classification => (
 				<option value={classification} key={classification}>{classification}</option>
 			))}
 		</select>
 		<select name="subClassifications" multiple>
-			{state.subClassifications.map(subClassification => (
+			{subClassifications.map(subClassification => (
 				<option value={subClassification} key={subClassification}>{subClassification}</option>
 			))}
 		</select>

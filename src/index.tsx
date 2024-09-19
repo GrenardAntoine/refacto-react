2cba01f0bea4e80037a9fb1170485e2df8af4822
alexandreguedj <alexandreguedj@gmail.com>
2024-09-19 11:27:26 +0200

    fix: fix error on render React app

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 // This file has to be left untouched
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App'
 import createStore from './redux/store'
-import { receiveDomains } from './redux/domains/actions';
+import { receiveDomains } from './redux/domains/actions'
 
-const store = createStore();
+const store = createStore()
 
 store.dispatch(receiveDomains([
-  'US_OK-WOK',
-  'FR_NK-WOL',
-  'FR_OK-NPP',
-  'EN_NK-NRP',
-  'EN_BL-WOL',
+	'US_OK-WOK',
+	'FR_NK-WOL',
+	'FR_OK-NPP',
+	'EN_NK-NRP',
+	'EN_BL-WOL',
 ]))
 
-ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+	<Provider store={store}><App /></Provider>,
+)
